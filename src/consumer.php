a2d6bb00c4caee07bb826efde2265c91a5545ff6
mazlanmohdnor <lan.psis@gmail.com>
2017-12-19 15:38:50 +0800

    update consumer

--- src/consumer.php
+++ src/consumer.php
@@ -10,15 +10,17 @@ $pass = '4s4ROw5Zeqy_QhtNKQ5uXtDz2SVj3DBG';
 $vhost = 'bcoocfcv';
 
 $exchange = 'subscriber';
-$queue = 'upm-result';
+$queue = 'upm-applicant';
 
 $connection = new AMQPStreamConnection($host, $port, $user, $pass, $vhost);
 $channel = $connection->channel();
 
 $channel->queue_declare($queue, false, true, false, false);
 
-$channel->exchange_declare($exchange, 'direct', false, true, false);
-$channel->queue_bind($queue, $exchange);
+// $channel->exchange_declare($exchange, 'direct', false, true, false);
+// $channel->queue_bind($queue, $exchange);
+
+
 
 //process the message from MQ
 function process_message($message)
@@ -37,17 +39,17 @@ function process_message($message)
     }
     // echo $message->body;
     $messages = json_decode($message->body);
-    foreach ($messages as $messageBody) {
+    // foreach ($messages as $messageBody) {
 
-        $ic = $messageBody->ic;
-        $name = $messageBody->name;
-        $phone = $messageBody->phone;
-        $address = str_replace(",", " ", (str_replace("\n", " ", $messageBody->address)));
-        $result = $messageBody->result;
-        $status = $messageBody->status;
+        $ic = $messages->ic;
+        $name = $messages->name;
+        $phone = $messages->phone;
+        $address = str_replace(",", " ", (str_replace("\n", " ", $messages->address)));
+        $result = $messages->result;
+        // $status = $messageBody->status;
 
-       $sql = "INSERT INTO studentsuccess (ic,name,phone,address,result,status)
-    VALUES ('$ic','$name','$phone','$address','$result','$status')";
+       $sql = "INSERT INTO studentsuccess (ic,name,phone,address,result)
+    VALUES ('$ic','$name','$phone','$address','$result')";
 
 if ($con->query($sql) === true) {
     echo "New record created successfully" . PHP_EOL;
@@ -58,13 +60,13 @@ if ($con->query($sql) === true) {
 
         // echo 'consumed data: ' . $messageBody . PHP_EOL;
 
-    }
+    // }
    
     mysqli_close($con);
 
-    if ($message->body === 'quit') {
-        $$message->delivery_info['channel']->basic_cancel($message->delivery_info['consumer_tag']);
-    }
+    // if ($message->body === 'quit') {
+    //     $$message->delivery_info['channel']->basic_cancel($message->delivery_info['consumer_tag']);
+    // }
 
 }
 /*
@@ -76,17 +78,20 @@ exclusive: Request exclusive consumer access, meaning only this consumer can acc
 nowait:
 callback: A PHP Callback
  */
+$callback = function ($msg) {
+    echo " [x] Received ", $msg->body, "\n";
+    process_message($msg);
+};
 
-$consumerTag = 'enterprise_consumer';
-$channel->basic_consume($queue, $consumerTag, false, false, false, false, 'process_message');
 
-function shutdown($channel, $connection)
-{
-    $channel->close();
-    $connection->close();
-}
-register_shutdown_function('shutdown', $channel, $connection);
+// $consumerTag = 'enterprise_consumer';
+$channel->basic_consume($queue, '', false, true, false, false, $callback);
 // Loop as long as the channel has callbacks registered
 while (count($channel->callbacks)) {
     $channel->wait();
 }
+
+
+    $channel->close();
+    $connection->close();
+
